c4c726ab2da7dc862a9ad874863a6aa206567942
Santiago Quiroga <quiroga.default.santiago@gmail.com>
2024-02-20 04:18:04 -0400

    add documentation for hanoi function

--- lib/Hanoi.hs
+++ lib/Hanoi.hs
@@ -1,5 +1,15 @@
 module Hanoi (hanoi) where
 
+-- Solves the Tower of Hanoi puzzle for a given number of disks.
+-- 
+-- The 'hanoi' function takes four arguments:
+-- * The number of disks to be moved.
+-- * The name of the source peg.
+-- * The name of the auxiliary peg.
+-- * The name of the destination peg.
+-- 
+-- It returns a list of pairs representing the moves to solve the puzzle.
+-- Each pair consists of the name of the source peg and the name of the destination peg.
 hanoi :: Int -> String -> String -> String -> [(String, String)]
 hanoi 0 _ _ _ = []
 hanoi n a b c = hanoi (n - 1) a c b ++ [(a, c)] ++ hanoi (n - 1) b a c
